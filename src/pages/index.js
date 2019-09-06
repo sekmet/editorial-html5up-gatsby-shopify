0290ac1c135f5f4230dbca93465821d43aa351c8
Carlos Guimaraes <sekmet@open4g.com>
2019-09-06 13:29:07 -0300

    blogify updates

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,11 +4,27 @@ import React from "react"
 import Banner from "../components/banner/"
 import Features from "../components/features/"
 //import Articles from "../components/articles/"
-//import Articles from "gatsby-theme-blogify/src/components/articles"
+import Articles from "gatsby-theme-blogify/src/components/articles"
 //import ProductGrid from '../components/products'
 //import Image from "../components/image"
 import SEO from "../components/seo"
 
+const blogOptions = require("../../static/admin/blog_options")
+
+const ConvertKeysToLowerCase = (obj) => {
+    var output = {};
+    for (var i in obj) {
+        if (Object.prototype.toString.apply(obj[i]) === '[object Object]') {
+            output[i.toLowerCase()] = ConvertKeysToLowerCase(obj[i]);
+        }else if(Object.prototype.toString.apply(obj[i]) === '[object Array]'){
+            output[i.toLowerCase()]=[];
+            output[i.toLowerCase()].push(ConvertKeysToLowerCase(obj[i][0]));
+        } else {
+            output[i.toLowerCase()] = obj[i];
+        }
+    }
+    return output;
+}
 
 
 const IndexPage = () => (
@@ -21,8 +37,8 @@ const IndexPage = () => (
     {/* Section*/}
     <Features />
 
-    {/* Section
-    <Articles />*/}
+    {/* Section*/}
+    <Articles pageContext={ConvertKeysToLowerCase(blogOptions)} />
   </>
 )
 
